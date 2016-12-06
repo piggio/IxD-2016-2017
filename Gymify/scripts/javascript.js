5f8c8c8e2ec8a6e5c20610955dad184e3c7d796f
piggio <leonardo.alfonsi@iclud.com>
2016-12-06 17:12:43 +0100

    lezione js

---
+++ Gymify/scripts/javascript.js
@@ -0,0 +1,34 @@
+
+
+var matteogentili = 0 ;
+
+
+
+console.log(matteogentili);
+
+
+console.log(window);
+
+
+console.log(matteogentili+44);
+
+
+var mancioli = 34 ;
+
+console.log(mancioli-matteogentili);
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+
+
+
+
+
+
+
+
+
+
